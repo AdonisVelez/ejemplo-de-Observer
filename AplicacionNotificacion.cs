4e4d654134305c02fd768e2fd9f190ea29a3078c
AdonisVelez <106783832+AdonisVelez@users.noreply.github.com>
2022-07-21 23:01:23 -0500

    Add files via upload

---
+++ AplicacionNotificacion.cs
@@ -0,0 +1,43 @@
+public class AplicacionNotificacion
+{
+    private readonly List<IObserver<Aplicacion>> observers;
+    public List<Aplicacion> aplicacions {get; set;}
+
+    public AplicacionNotificacion()
+    {
+        observers = new();
+        aplicacions = new();
+    }
+
+    public IDisposable Subcribir (IObserver<Aplicacion> observer)
+    {
+        if ( !observers.Contains(observer))
+        {
+            observers.Add(observer);
+            
+            foreach (var item in aplicacions)
+            {
+                observer.OnNext(item);
+            }
+        }
+        return new Unsubcripcion(observers, observer);
+    }
+    public void AddAplicaciones (Aplicacion app)
+    {
+        aplicacions.Add(app);
+
+        foreach (var observer in observers)
+        {
+            observer.OnNext(app);
+        }
+    }
+
+    public void CerrarAplicacion()
+    {
+        foreach (var observer in observers)
+        {
+            observer.OnCompleted();
+        }
+        observers.Clear();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Observer
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            var observer1 = new EspecialistaRH("Marco");
+            var observer2 = new EspecialistaRH("Maria");
+
+            var proveedor = new AplicacionNotificacion();
+
+            observer1.Subcribir(proveedor);
+            observer2.Subcribir(proveedor);
+            proveedor.AddAplicaciones(new(1,"Contador"));
+            proveedor.AddAplicaciones(new(2,"Cantante"));
+
+            observer1.ListaAplicacion();
+            observer2.ListaAplicacion();
+
+            observer2.Unsubcripcion();
+
+            Console.WriteLine();
+            Console.WriteLine($"{observer2.nombre} se ha eliminado su subcripcion");
+            Console.WriteLine();
+
+            proveedor.AddAplicaciones(new(3,"Sistemas"));
+
+            observer1.ListaAplicacion();
+            observer2.ListaAplicacion();
+
+            Console.WriteLine();
+
+            proveedor.CerrarAplicacion();
+        }
+    }
+}
